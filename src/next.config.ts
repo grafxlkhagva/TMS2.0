60ec6e411538925e074eae89c811d52b32645c5d
Lhagvasüren Otgonbayar <lkhagvasuren@trt.mn>
2025-08-10 07:53:00 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -23,6 +23,9 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
   // This is to allow the Next.js dev server to accept requests from the Studio editor.
+  experimental: {
+    //
+  },
   allowedDevOrigins: [
     '*.cluster-bg6uurscprhn6qxr6xwtrhvkf6.cloudworkstations.dev',
   ],
