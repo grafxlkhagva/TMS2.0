859c73fcdf0fbab9805c5ac6720005263ced5858
Lhagvasüren Otgonbayar <lkhagvasuren@trt.mn>
2025-08-16 07:57:07 +0000

    зурагийн url ийм байдалаар ирж байна энэ зөв байна уу : https://firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -9,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDVkZ4cvMGbIdKG6gOCD-kcmHjIThP_2E0",
   authDomain: "tumen-tech-tms-1.firebaseapp.com",
   projectId: "tumen-tech-tms-1",
-  storageBucket: "tumen-tech-tms-1.firebasestorage.app",
+  storageBucket: "tumen-tech-tms-1.appspot.com",
   messagingSenderId: "983460389511",
   appId: "1:983460389511:web:d4a4e47c7f660ff6788e22"
 };
