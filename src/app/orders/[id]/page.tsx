e509464c4307b1b8d623bc544c928cd8f2cb3e66
Lhagvasüren Otgonbayar <lkhagvasuren@trt.mn>
2025-09-04 02:21:39 +0000

    sheet-рүү илэээх товч дарахд гэрсан алдаа

--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -639,7 +639,7 @@ export default function OrderDetailPage() {
     try {
         const payload = {
             order,
-            orderItem,
+            orderItem: item,
             quote,
             allData
         };
