42c85d3d635bed99411f7bbe276355bcaadf354e
Lhagvasüren Otgonbayar <lkhagvasuren@trt.mn>
2025-12-09 11:37:30 +0000

    гэрээт тэвэрийн хэсэг үндсэндээ ажилгааны хувьд төгс боллоо. Харин UX UI

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -17,7 +17,6 @@ const badgeVariants = cva(
         outline: "text-foreground",
         success: "border-transparent bg-green-500 text-primary-foreground hover:bg-green-500/80",
         warning: "border-transparent bg-yellow-500 text-primary-foreground hover:bg-yellow-500/80",
-
       },
     },
     defaultVariants: {
